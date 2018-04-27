195f051ff2676cd3fdcd91c4706140065a045968
Alvaro Figueroa <alvaro@greencore.co.cr>
2018-04-27 11:28:52 -0600

    turn-on.sh: Se le pone TRUE por default

--- files/turn-on.sh
+++ files/turn-on.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ans=$(zenity --width=330 --height=300 --list  --text "¿Cuales equipos desea activar?" --checklist  --column "Seleccione" --column "Equipo" \
-	FALSE "Estacion #01 (6c:0b:84:09:ff:9e)" \
-	FALSE "Estacion #02 (6c:0b:84:09:ff:5c)" \
-	FALSE "Estacion #03 (6c:0b:84:09:fb:f5)" \
-	FALSE "Estacion #04 (6c:0b:84:0a:03:a6)" \
-	FALSE "Estacion #05 (6c:0b:84:09:ff:dd)" \
-	FALSE "Estacion #06 (6c:0b:84:09:ff:76)" \
-	FALSE "Estacion #07 (6c:0b:84:09:ff:4e)" \
-	FALSE "Estacion #08 (6c:0b:84:08:f2:56)" \
+	TRUE "Estacion #01 (6c:0b:84:09:ff:9e)" \
+	TRUE "Estacion #02 (6c:0b:84:09:ff:5c)" \
+	TRUE "Estacion #03 (6c:0b:84:09:fb:f5)" \
+	TRUE "Estacion #04 (6c:0b:84:0a:03:a6)" \
+	TRUE "Estacion #05 (6c:0b:84:09:ff:dd)" \
+	TRUE "Estacion #06 (6c:0b:84:09:ff:76)" \
+	TRUE "Estacion #07 (6c:0b:84:09:ff:4e)" \
+	TRUE "Estacion #08 (6c:0b:84:08:f2:56)" \
 	 --separator=";" 2>/dev/null)
 
 cant=$(echo $ans | gawk -F\; '{print NF}')
